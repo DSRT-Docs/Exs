100b0d0f7bf5bff1d37caa588bd16d9167d87aca
DSRT-Docs <103884982+DSRT-Docs@users.noreply.github.com>
2025-12-27 17:51:07 +0700

    Update SystemInfo.cpp

--- src/private/platform/SystemInfo.cpp
+++ src/private/platform/SystemInfo.cpp
@@ -1,7 +1,6 @@
 // Copyright 2024 Exs Framework. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0 (the "License");
 
-
 #include "SystemInfo.h"
 #include <chrono>
 #include <iostream>
