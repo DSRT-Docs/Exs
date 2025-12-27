a9464a679c072424eaec48bb26a829d4da6ef075
DSRT-Docs <103884982+DSRT-Docs@users.noreply.github.com>
2025-12-27 17:54:23 +0700

    Update Platform.cpp

--- src/private/platform/Platform.cpp
+++ src/private/platform/Platform.cpp
@@ -1,3 +1,6 @@
+// Copyright 2024 Exs Framework. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+
 #include "Platform.h"
 #include <chrono>
 #include <iostream>
